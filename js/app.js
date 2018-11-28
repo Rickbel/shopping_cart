e5867ccf5bd86b05692f4ae30da0b825929d5c16
Wilson Ricardo Humerez Hidalgo <rickbel_94@outlook.com>
2018-11-28 18:16:23 -0400

    fixing cart

--- js/app.js
+++ js/app.js
@@ -21,11 +21,54 @@ btn.addEventListener("click", function(event) {
     if(event.target.parentElement.classList.contains("store-item-icon")){
         let fullPath = 
         event.target.parentElement.previousElementSibling.src;
-        let pos = fullPath.indexOf("img");
-        console.log(pos);
-    }
- });
-});
+        let pos = fullPath.indexOf("img")+3;
+        let partPath = fullPath.slice(pos);
 
+        const item ={}; //objeto vacio
+        item.img = `img-cart${partPath}`;
+        let name = event.target.parentElement.parentElement.nextElementSibling.children[0].children[0].textContent;
+        item.name = name;
 
+        let price = event.target.parentElement.parentElement.nextElementSibling.children[0].children[1].textContent;
+        let finalPrice = price.slice(1).trim();
+
+        item.price = finalPrice;
+        //console.log(finalPrice);
+        
+        //console.log(name);
+        
+        //console.log(item);
+        
+        const cartItem = document.createElement('div');
+        cartItem.classList.add("cart-item",
+        "d-flex", 
+        "justify-content-between",
+        "text-capitalize",
+        "my-3");
+
+        cartItem.innerHTML = `
+            <img src="${item.img}" class="img-fluid rounded-circle" id="item-img" alt="">
+                    <div class="item-text">
+
+                        <p id="cart-item-title" class="font-weight-bold mb-0">${item.name}</p>
+                        <span>$</span>
+                        <span id="cart-item-price" class="cart-item-price" class="mb-0">${item.price}</span>
+                    </div>
+                    <a href="#" id='cart-item-remove' class="cart-item-remove">
+                        <i class="fas fa-trash"></i>
+                    </a>
+                    </div>
+                    `;
+        //select cart
+        const cart = document.getElementById("cart");
+        const total = document.queryCommandEnabled(".cart-total-container");
+        
+        cart.insertBefore(cartItem, total);
+        alert("se agrego al carro");
+          
+          }
+       });
+      });
+      
+      
 })();
