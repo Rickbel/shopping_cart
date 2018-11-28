3f3e81d42c7bbb9a2f6d195dbcf4ffd223620c2e
Wilson Ricardo Humerez Hidalgo <rickbel_94@outlook.com>
2018-11-28 16:54:02 -0400

    funcionalidad del carrito

--- js/app.js
+++ js/app.js
@@ -1,11 +1,31 @@
 // show cart
 
 (function () {
-const cartInfo=document.getElementById('cart-info');
-const cart  = document.getElementById('cart');
+const cartInfo=document.getElementById("cart-info");
+const cart  = document.getElementById("cart");
 
-cartInfo.addEventListener('click',function () {
-    cart.classList.toggle('show-cart');
+cartInfo.addEventListener("click",function () {
+    cart.classList.toggle("show-cart");
 });
 })();
 
+// add items to the cart
+(function () {
+    
+const cartBtn = document.querySelectorAll(".store-item-icon");
+
+cartBtn.forEach(function(btn) {
+btn.addEventListener("click", function(event) {
+    //console.log(event,target);
+
+    if(event.target.parentElement.classList.contains("store-item-icon")){
+        let fullPath = 
+        event.target.parentElement.previousElementSibling.src;
+        let pos = fullPath.indexOf("img");
+        console.log(pos);
+    }
+ });
+});
+
+
+})();
